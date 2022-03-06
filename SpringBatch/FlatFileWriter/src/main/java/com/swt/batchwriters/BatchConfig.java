79d660b553763deaa2751cb534172522a440a5dc
Manoj Joshi <manoj.joshi@smoothstack.com>
2022-03-06 17:16:01 +0000

    flat file writer

---
+++ SpringBatch/FlatFileWriter/src/main/java/com/swt/batchwriters/BatchConfig.java
@@ -0,0 +1,124 @@
+package com.swt.batchwriters;
+
+import com.swt.batchwriters.model.Product;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileFooterCallback;
+import org.springframework.batch.item.file.FlatFileHeaderCallback;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@EnableBatchProcessing
+@Configuration
+public class BatchConfig {
+
+    @Autowired
+    private StepBuilderFactory steps;
+
+    @Autowired
+    private JobBuilderFactory jobs;
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader reader(
+            @Value( "#{jobParameters['fileInput']}"  ) FileSystemResource inputFile
+            ){
+
+        FlatFileItemReader reader = new FlatFileItemReader();
+        reader.setResource(inputFile);
+        reader.setLinesToSkip(1);
+        reader.setLineMapper(new DefaultLineMapper(){
+            {
+                setFieldSetMapper(new BeanWrapperFieldSetMapper(){
+                    {
+                        setTargetType(Product.class);
+                    }
+                });
+
+                setLineTokenizer(new DelimitedLineTokenizer(){
+                    {
+                        setNames(new String[]{"productId","prodName","productDesc"  ,"price","unit"});
+                        setDelimiter(",");
+                    }
+                });
+            }
+        });
+
+        return reader;
+    }
+
+   @Bean
+   @StepScope
+   public FlatFileItemWriter flatFileItemWriter(
+           @Value("#{jobParameters['fileOutput']}" )FileSystemResource outputFile
+     ){
+        FlatFileItemWriter writer = new FlatFileItemWriter();
+
+        writer.setResource(outputFile);
+        writer.setLineAggregator( new DelimitedLineAggregator(){
+            {
+                setDelimiter("|");
+                setFieldExtractor(new BeanWrapperFieldExtractor(){
+                    {
+                        setNames(new String[]{"productId","prodName","productDesc","price","unit" });
+                    }
+                });
+            }
+        });
+
+        // how to write the header
+        writer.setHeaderCallback(new FlatFileHeaderCallback() {
+            @Override
+            public void writeHeader(Writer writer) throws IOException {
+                writer.write("productID,productName,ProductDesc,price,unit");
+            }
+        });
+
+        writer.setAppendAllowed(false);
+
+        writer.setFooterCallback(new FlatFileFooterCallback() {
+            @Override
+            public void writeFooter(Writer writer) throws IOException {
+                writer.write(" The file is created at " + new SimpleDateFormat().format(new Date()));
+            }
+        });
+        return writer;
+   }
+
+
+   @Bean
+   public Step writerStep(){
+        return steps.get("writerStep")
+                .<Product,Product>chunk(3)
+                .reader(reader(null))
+                .writer(flatFileItemWriter(null))
+                .build();
+   }
+
+   @Bean
+   public Job writerJob(){
+        return jobs.get("writerJob")
+                .start(writerStep())
+                .build();
+   }
+
+}
