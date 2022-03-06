eebb150d57b18530f626f6f3c590cbecb4eb556a
Manoj Joshi <manoj.joshi@smoothstack.com>
2022-03-06 02:03:29 +0000

    adding json item reader

---
+++ SpringBatch/JsonItemReader/src/main/java/com/smoothstack/springbatch/JsonItemReader/JsonItemReaderApplication.java
@@ -0,0 +1,12 @@
+package com.smoothstack.springbatch.JsonItemReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonItemReaderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JsonItemReaderApplication.class, args);
+    }
+
+}
---
+++ SpringBatch/JsonItemReader/src/main/java/com/smoothstack/springbatch/JsonItemReader/config/BatchConfiguration.java
@@ -0,0 +1,62 @@
+package com.smoothstack.springbatch.JsonItemReader.config;
+
+import com.smoothstack.springbatch.JsonItemReader.listener.HwJobExecutionListener;
+import com.smoothstack.springbatch.JsonItemReader.listener.HwStepExecutionListener;
+import com.smoothstack.springbatch.JsonItemReader.model.Product;
+import com.smoothstack.springbatch.JsonItemReader.writer.ConsoleItemWriter;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.json.JacksonJsonObjectReader;
+import org.springframework.batch.item.json.JsonItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.FileSystemResource;
+
+
+@EnableBatchProcessing
+@org.springframework.context.annotation.Configuration
+public class BatchConfiguration {
+
+    @Autowired
+    private JobBuilderFactory jobs;
+
+    @Autowired
+    private StepBuilderFactory steps;
+
+    @Autowired
+    private HwJobExecutionListener hwJobExecutionListener;
+
+    @Autowired
+    private HwStepExecutionListener hwStepExecutionListener;
+
+    @StepScope
+    @Bean
+    public JsonItemReader jsonItemReader(@Value( "#{jobParameters['jsonFile']}" ) FileSystemResource inputFile){
+        JsonItemReader reader = new JsonItemReader(inputFile, new JacksonJsonObjectReader(Product.class));
+        return reader;
+    }
+
+    @Bean
+    public Step jsonFileStep(){
+        return steps.get("jsonFileStep").
+                        <Integer,Integer>chunk(3)
+                 .reader(jsonItemReader( null ))
+                .writer(new ConsoleItemWriter())
+                .build();
+    }
+
+    @Bean
+    public Job jsonFileJob(){
+        return jobs.get("jsonFileJob")
+                .incrementer(new RunIdIncrementer())
+                .listener(hwJobExecutionListener)
+                .start(jsonFileStep())
+                .build();
+    }
+}
