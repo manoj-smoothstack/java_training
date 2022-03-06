e8f1954d3aeeb91f289ea612fc5fa46afdf8f028
Manoj Joshi <manoj.joshi@smoothstack.com>
2022-03-06 21:01:22 +0000

    skip invalid records

---
+++ SpringBatch/SkipInvalidRecords/src/main/java/com/smoothstack/springbatch/skipinvalid/SkipInvalidRecordsApplication.java
@@ -0,0 +1,11 @@
+package com.smoothstack.springbatch.skipinvalid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkipInvalidRecordsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SkipInvalidRecordsApplication.class, args);
+    }
+}
