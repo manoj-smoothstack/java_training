574905f30d72084730f990fff94fb24e70d251f7
Manoj Joshi <manoj.joshi@smoothstack.com>
2022-04-12 13:29:22 -0700

    variable args

---
+++ basic/src/com/smoothstack/misc/VarargsEx.java
@@ -0,0 +1,28 @@
+package com.smoothstack.misc;
+
+public class VarargsEx {
+    static void fun(int... a) {
+        System.out.println("Number of arguments: " + a.length);
+
+        // using for each loop to display contents of a
+        for (int i : a)
+            System.out.print(i + " ");
+        System.out.println();
+    }
+
+    // Driver code
+    public static void main(String args[]) {
+        // Calling the varargs method with
+        // different number of parameters
+
+        // one parameter
+        fun(100);
+
+        // four parameters
+        fun(1, 2, 3, 4);
+
+        // no parameter
+        fun();
+    }
+
+}
